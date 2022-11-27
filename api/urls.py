bb069e23073a3d140d2e2b313b579e1474ad1245
jw122 <thejuliawu@gmail.com>
2022-11-27 13:15:42 -0500

    add models

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,8 @@
-from django.contrib import admin
-from django.urls import path
-from django.conf.urls import include
+from django.urls import path, include
 from rest_framework import routers
 
 
-router = routers.DefaultRouter
+router = routers.DefaultRouter()
 urlpatterns = [
     path("", include(router.urls))
 ]
