dc91ed7ac7198c4e865b77d202e5280f968a8e67
jw122 <thejuliawu@gmail.com>
2022-11-27 14:50:53 -0500

    average rating

--- api/models.py
+++ api/models.py
@@ -1,9 +1,26 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MaxValueValidator, MinValueValidator
+
+
 class Movie(models.Model):
     title = models.CharField(max_length=32)
     description = models.TextField(max_length=360)
+
+    def number_of_ratings(self):
+        # get ratings for this movie and see how many ratings there are
+        ratings = Rating.objects.filter(movie=self)  # select all ratings in the db that have this movie
+        return len(ratings)
+
+    def average_rating(self):
+        ratings = Rating.objects.filter(movie=self)
+        sum = 0
+        for rating in ratings:
+            sum += rating.stars
+
+        if len(ratings) > 0:
+            return sum/len(ratings)
+        return 0
 
 class Rating(models.Model):
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
